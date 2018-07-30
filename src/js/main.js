67f35deda9b7d8e0fdf1fdffdace0432e7db857b
Joonas Reinikka <joonas.reinikka@gmail.com>
2018-07-31 02:19:45 +0300

    Change music

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,21 @@
-import { Howl, Howler } from "howler";
+import { Howl, Howler } from 'howler';
 
-const NeonRidingUrl = require("../assets/music/Neon_Riding.mp3");
-const playButton = document.getElementById("playButton");
+const musicUrl = require('../assets/music/Herr_Doktor_-_06_-_Revenge.mp3');
+const playButton = document.getElementById('playButton');
 
 const music = new Howl({
-  src: [NeonRidingUrl],
+  src: [musicUrl],
   html5: true,
   preload: false,
   loop: true,
   onload: () => music.play(),
-  onplay: () => (playButton.innerHTML = "> Pause"),
-  onpause: () => (playButton.innerHTML = "> Play")
+  onplay: () => (playButton.innerHTML = '> Pause'),
+  onpause: () => (playButton.innerHTML = '> Play'),
 });
 
-playButton.addEventListener("click", () => {
-  if (music.state() === "unloaded") {
-    playButton.innerHTML = "> Loading...";
+playButton.addEventListener('click', () => {
+  if (music.state() === 'unloaded') {
+    playButton.innerHTML = '> Loading...';
     music.load();
   } else {
     if (music.playing()) {
